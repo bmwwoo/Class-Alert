b1ee5748e1a36fd45fad79e8021dc5568ed566f9
Brandon Woo <bmw.woo@gmail.com>
2014-11-20 16:19:28 -0800

    can write out the data from each class now

--- scraperScript.py
+++ scraperScript.py
@@ -5,8 +5,8 @@ from pprint import pprint
 import courseGraphData
 
 def CourseLinks():
-	term = 'W15'
-	courseLinkFilename =  './ClassLinks/allLinks' + term + '.json'
+	TERM = 'W15'
+	courseLinkFilename =  './ClassLinks/allLinks' + TERM + '.json'
 	courseLinkJSON = open(courseLinkFilename)
 	allCourseLinks = json.load(courseLinkJSON)
 	return allCourseLinks
@@ -22,17 +22,40 @@ courseDict = {}
 classes = []
 for subject in allCourseLinks['Computer Science']:
 	classes.append(courseGraphData.createCourse(subject))
-courseDict['Computer Science'] = classes
-#pprint(courseDict)
-for team in courseDict['Computer Science']:
-	for a in team:
+courseDict["Computer Science"] = classes
+
+
+
+'''
+courseDict = {}
+courseLoad = open("compsci.json")
+courseDict = json.load(courseLoad)
+'''
+
+
+for compsci in courseDict['Computer Science']:
+	for theClass in compsci:
+		if compsci[theClass]:
+			for theLecture in compsci[theClass]:
+				print theLecture['title'] + '\t' + theLecture['professor'] + '\t' + str(theLecture['enrolledCapacity']) + '\t' + str(theLecture['enrolled'])
+		
+'''
 		for b in a:
-			pprint(b)
-			'''for c in b:
+			print b
+			for c in b:
 				for metaData in c:
 					#print "Title:\t%s\nEnrolled Capacity:\t%d\nEnrolled Now:\t%d" % (metaData['title'], metaData['enrolledCapacity'], metaData['enrolled'])
+
+#pprint( courseDict['Computer Science'][2]['COM SCI 32 INTRO TO COM SCI 2'][0]['enrolled'] )
 '''
-pprint( courseDict['Computer Science'][2]['COM SCI 32 INTRO TO COM SCI 2'][0]['enrolled'] )
+'''
+for(int i=0; i<numItems; i++)
+{
+	//For each Computer Science in courseDict
+		//For each key (there will only be one key, i.e. COM SCI 32 INTO TO COM SCI 2)
+			//For each item in the array of each key
+				//print enrolled
+}'''
 #	print "Title:\t%s\nEnrolled Capacity:\t%d\nEnrolled Now\t%d" % (team['title'], team['enrolledCapacity'], team['enrolled'])
 
 #client.close()
